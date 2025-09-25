ad05b8ad05e3c2a930f445ef2c77748aeaca8a8e
shaunschofield <shaunschofield123@gmail.com>
2025-09-25 17:43:39 +1000

    CRUD website with REST api

--- sqlite_web_server/src/routes/todoRoutes.js
+++ sqlite_web_server/src/routes/todoRoutes.js
@@ -15,17 +15,36 @@ router.get('/', (req, res) => {
 //Create a new todo
 router.post('/', (req, res) => {
 
+    const { task } = req.body
+    const insertTodo = db.prepare(`INSERT INTO todos (user_id, task) VALUES
+    (?, ?)`)
+    const result = insertTodo.run(req.userId, task)
+
+    res.json({ id: result.lastInsertRowid, task, completed: 0 })
 })
 
 //Update a todo (match up todos)
 router.put('/:id', (req, res) => {
 
+    const { completed } = req.body
+    const { id } = req.params
+    const { page } = req.query
+    
+    const updatedTodo = db.prepare(`UPDATE todos SET completed = ? WHERE id = ?`)
+    
+    updatedTodo.run(completed, id)
+
+    res.json({ message: "Todo completed"})
 })
 
 // Delete a todo
 router.delete('/:id', (req, res) => {
 
-    
+    const { id } = req.params
+    const userId = req.userId
+    const deleteTodo = db.prepare(`DELETE FROM todos WHERE id = ? AND user_id = ?`)
+    deleteTodo.run(id, userId)
+    res.send({ message: "Deleted"})
 })
 
 export default router
